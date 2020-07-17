94f290c954438dcf3410b00c73b337beeaf1fef6
Krizza Bullecer <krizzabullecer@gmail.com>
2020-07-18 02:44:34 +0800

    complete bigweather

---
+++ src/components/BigWeather.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="big-weather">
+    <img :src="weatherImg" :alt="weather" />
+    <h1>
+      {{temperature}}
+      <span>°C</span>
+    </h1>
+    <h3>{{weather}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BigWeather',
+  props: {
+    temperature: String,
+    weather: String,
+  },
+  computed: {
+    weatherImg() {
+      return `../assets/${this.weather.split(' ').join('')}.png`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap");
+
+.big-weather {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  h1 {
+    margin: 20px 0;
+    font-size: 120px;
+    color: $white;
+
+    span {
+      font-family: "Noto Sans JP", sans-serif;
+      font-size: 40px;
+      font-weight: 400;
+      color: $grey;
+      margin-left: -20px;
+    }
+  }
+
+  h3 {
+    font-size: 28px;
+  }
+}
+</style>
