1b8fc2d094baed5fc8f0f1219d2fdcdee0388174
Krizza Bullecer <krizzabullecer@gmail.com>
2020-07-19 07:28:59 +0800

    complete error handling and loading

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
+  <!-- eslint-disable max-len -->
   <div class="app">
     <div class="sidebar">
       <div class="sidebar-wrap">
-        <search :isOpen="isSearch" @close="toggleSearch" />
+        <search :isOpen="isSearch" :locations="locations" @close="toggleSearch" />
         <div class="sidebar-actions">
           <mdb-btn class="sidebar-search" @click="toggleSearch(true)">Search for places</mdb-btn>
           <mdb-tooltip material trigger="hover" :options="{placement: 'bottom'}">
             <span slot="tip">Current location</span>
-            <mdb-btn slot="reference" class="sidebar-gps">
+            <mdb-btn slot="reference" class="sidebar-gps" @click="getCurrentLocation">
               <i class="material-icons">gps_fixed</i>
             </mdb-btn>
           </mdb-tooltip>
         </div>
 
-        <big-weather temperature="15" weather="Shower" />
+        <div v-if="!weather.consolidated_weather || loading" class="sidebar-error">
+          <i class="material-icons">cloud_off</i>
+          <spinner v-if="!weather.consolidated_weather || loading"></spinner>
+          <div v-if="errorStr">
+            <h3>Oops!</h3>
+            <p>Something went wrong!</p>
+          </div>
+        </div>
+
+        <div v-if="weather.consolidated_weather && !loading">
+          <big-weather
+            :temperature="inCelsius ? weather.consolidated_weather[0].the_temp : toFahrenheit(weather.consolidated_weather[0].the_temp)"
+            :weather="weather && weather.consolidated_weather[0].weather_state_name"
+            :isCelsius="inCelsius"
+          />
+        </div>
 
         <div class="weather-today-details">
           <p class="weather-date">
@@ -23,64 +39,103 @@
           </p>
           <p class="weather-location">
             <i class="material-icons">location_on</i>
-            {{location}}
+            {{weather.title ? weather.title : 'Loading...'}}
           </p>
         </div>
       </div>
     </div>
+
     <div class="main">
-      <div class="cel-fan">
-        <mdb-btn class="celsius" @click="toggleFormat(true)">℃</mdb-btn>
-        <mdb-btn class="farenheight" @click="toggleFormat(false)">℉</mdb-btn>
+      <loader v-if="!weather.consolidated_weather || loading"></loader>
+
+      <div v-if="errorStr" class="main-error">
+        <h2>Location Not Found</h2>
+        <p>To fix this issue, please:</p>
+        <ul>
+          <li>Allow location services in your browser</li>
+          <li>Use the search bar on the top-left instead</li>
+        </ul>
       </div>
 
-      <div class="weather-forecast">
-        <small-weather :date="new Date()" weather="Sleet" max="16" min="11" />
-        <small-weather :date="new Date()" weather="Sleet" max="16" min="11" />
-        <small-weather :date="new Date()" weather="Sleet" max="16" min="11" />
-        <small-weather :date="new Date()" weather="Sleet" max="16" min="11" />
-        <small-weather :date="new Date()" weather="Sleet" max="16" min="11" />
+      <div v-if="weather.consolidated_weather && !loading">
+        <div class="cel-fan">
+          <mdb-btn
+            type="button"
+            class="celsius"
+            :class="{active: inCelsius}"
+            @click="toggleFormat(true)"
+          >℃</mdb-btn>
+          <mdb-btn
+            type="button"
+            class="farenheight"
+            :class="{active: !inCelsius}"
+            @click="toggleFormat(false)"
+          >℉</mdb-btn>
+        </div>
+
+        <div class="weather-forecast">
+          <small-weather
+            v-for="i in 5"
+            :key="i"
+            :date="new Date(weather.consolidated_weather[i].applicable_date)"
+            :weather="weather.consolidated_weather[i].weather_state_name"
+            :isCelsius="inCelsius"
+            :max="inCelsius ? weather.consolidated_weather[i].max_temp : toFahrenheit(weather.consolidated_weather[i].max_temp)"
+            :min="inCelsius ? weather.consolidated_weather[i].min_temp : toFahrenheit(weather.consolidated_weather[i].min_temp)"
+          />
+        </div>
+
+        <h4>Today's Highlights</h4>
+        <div class="weather-highlight">
+          <card
+            title="Wind status"
+            :direction="weather.consolidated_weather[0].wind_direction_compass"
+            :directionNum="weather.consolidated_weather[0].wind_direction"
+          >
+            <template v-slot:value>
+              {{Math.round(weather.consolidated_weather[0].wind_speed)}}
+              <span>mph</span>
+            </template>
+          </card>
+
+          <card title="Humidity" :percent="weather.consolidated_weather[0].humidity">
+            <template v-slot:value>
+              {{weather.consolidated_weather[0].humidity}}
+              <span>%</span>
+            </template>
+          </card>
+
+          <card title="Visibility">
+            <template v-slot:value>
+              {{Math.round(weather.consolidated_weather[0].visibility)}}
+              <span>miles</span>
+            </template>
+          </card>
+
+          <card title="Air Pressure ">
+            <template v-slot:value>
+              {{Math.round(weather.consolidated_weather[0].air_pressure)}}
+              <span>mb</span>
+            </template>
+          </card>
+        </div>
       </div>
 
-      <h3>Today's Highlights</h3>
-      <div class="weather-highlight">
-        <card title="Wind status" direction="WSW">
-          <template v-slot:value>
-            7
-            <span>mph</span>
-          </template>
-        </card>
-
-        <card title="Humidity" percent="84">
-          <template v-slot:value>
-            84
-            <span>%</span>
-          </template>
-        </card>
-
-        <card title="Visibility">
-          <template v-slot:value>
-            6,4
-            <span>miles</span>
-          </template>
-        </card>
-
-        <card title="Air Pressure ">
-          <template v-slot:value>
-            998
-            <span>mb</span>
-          </template>
-        </card>
+      <div class="footer">
+        <p>Nicole Bullecer © DevChallenges.io</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
 import {
   mdbTooltip, mdbBtn,
 } from 'mdbvue';
 import { format } from 'date-fns';
+import Spinner from '@/components/RoundSpinner.vue';
+import Loader from '@/components/DribbleDot.vue';
 import Search from '@/components/Search.vue';
 import BigWeather from '@/components/BigWeather.vue';
 import SmallWeather from '@/components/SmallWeather.vue';
@@ -91,6 +146,8 @@ export default {
   components: {
     mdbTooltip,
     mdbBtn,
+    Spinner,
+    Loader,
     Search,
     BigWeather,
     SmallWeather,
@@ -99,26 +156,68 @@ export default {
   data() {
     return {
       dateNow: Date.now(),
-      location: 'Manila',
       isSearch: false,
+      inCelsius: true,
+      isGettingLocation: false,
+      errorStr: '',
     };
   },
   computed: {
     today() {
       return format(this.dateNow, 'E, d MMM');
     },
+    ...mapGetters({
+      locations: 'gettersLocations',
+      weather: 'gettersWeather',
+      loading: 'gettersIsLoading',
+    }),
+  },
+  watch: {
+    locations(newVal) {
+      this.getWeather(newVal[0].woeid);
+    },
   },
   methods: {
     toggleSearch(isOpen) {
       this.isSearch = isOpen;
     },
+    toggleFormat(isCelsius) {
+      this.inCelsius = isCelsius;
+    },
+    toFahrenheit(num) {
+      return ((num * 9) / 5) + 32;
+    },
+    getCurrentLocation() {
+      if (!('geolocation' in navigator)) {
+        this.errorStr = 'Location services is not available.';
+        return;
+      }
+
+      this.isGettingLocation = true;
+      navigator.geolocation.getCurrentPosition((pos) => {
+        this.isGettingLocation = false;
+        this.getLocations({
+          latitude: `${pos.coords.latitude}`,
+          longitude: `${pos.coords.longitude}`,
+        });
+      }, () => {
+        this.isGettingLocation = false;
+        this.errorStr = 'Could not get current location. Please allow the location service on your browser.';
+      });
+    },
+    ...mapActions({
+      getLocations: 'getLocations',
+      getWeather: 'getWeather',
+    }),
+  },
+  created() {
+    this.getCurrentLocation();
   },
 };
 </script>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Raleway:wght@500;600;700&display=swap");
-@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+JP&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Noto+Sans+JP&&family=Raleway:wght@500;600;700&display=swap");
 
 * {
   font-family: Raleway, sans-serif;
@@ -153,6 +252,33 @@ button {
   color: $grey;
   background: $primary;
   overflow: hidden;
+
+  .sidebar-error {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    height: 100%;
+    margin-top: 70px;
+
+    i {
+      font-size: 200px;
+    }
+
+    div {
+      text-align: center;
+
+      h2 {
+        font-size: 60px;
+        font-weight: 700;
+        margin-bottom: 5px;
+      }
+
+      p {
+        font-size: 18px;
+      }
+    }
+  }
 
   &::before {
     content: "";
@@ -228,7 +354,22 @@ button {
   padding: 42px;
   background: $dark;
 
-  h3 {
+  .main-error {
+    width: 100%;
+    height: calc(100vh - 84px);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h2 {
+      font-size: 54px;
+      font-weight: 700;
+      margin-bottom: 5px;
+    }
+  }
+
+  h4 {
     margin: 40px 14px 24px;
     font-size: 24px;
     font-weight: 700;
@@ -272,7 +413,19 @@ button {
   width: 100%;
 }
 
-@media (max-width: 576px) {
+.footer {
+  text-align: center;
+  margin: 40px auto 20px;
+
+  p {
+    color: $darkgrey;
+    font-family: Montserrat, sans-serif;
+    font-size: 14px;
+    font-weight: 600;
+  }
+}
+
+@media (max-width: 992px) {
   .sidebar {
     position: static;
     height: calc(100vh - 48px);
